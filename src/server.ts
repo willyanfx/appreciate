9e25a938cf1a7d75ce3a9a3de25fddb892d8906a
willyanfx <willyan.fx@gmail.com>
2021-12-14 10:57:33 -0300

    router

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,12 @@
 import cors from "cors";
 import express, { Router, Request, Response } from "express";
+import { router } from "./routes";
 
 const app = express();
-const route = Router();
 
 app.use(cors());
 app.use(express.json());
 
-route.get("/", (req: Request, res: Response) => {
-  res.json({ message: "hello world with Typescript" });
-});
-
-app.use(route);
+app.use(router);
 
 app.listen(3333, () => "server running on port 3333");

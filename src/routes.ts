e9a1d77d47c9083f61569f80b755802bfa2baae7
willyanfx <willyan.fx@gmail.com>
2021-12-22 16:43:03 -0300

    add controller, entities, reposotories, services

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,14 @@
 import { Router, Request, Response } from "express";
+import { CreateUserController } from "./controllers/CreateUserController";
 
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
   res.json({ message: "hello world with Typescript" });
 });
+
+const createUserController = new CreateUserController();
+
+router.post("/users", createUserController.handle);
 
 export { router };
